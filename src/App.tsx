3ce77e04a60c75097857760f28bba45d92c3536f
Kieran Bazeley <bazeleykieran@gmail.com>
2026-08-15 11:33:20 +0100

    init with board being rendered

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/index.css";
+import Board from "./components/Board";
+
+function App() {
+  return (
+    <main className="h-screen flex items-center justify-center">
+      <Board />
+    </main>
+  );
+}
+
+export default App;
